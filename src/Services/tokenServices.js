61bb140a03e436599e7fafc568106a723e82dce7
B.Lunsford <breanna.sirbin@gmail.com>
2020-12-08 15:13:09 -0700

    stub upped Services user and token

---
+++ src/Services/tokenServices.js
@@ -0,0 +1,22 @@
+function setToken () {
+
+}
+
+function getUserToken () {
+
+}
+
+function getToken () {
+
+}
+
+function removeToken () {
+
+}
+
+export {
+    setToken,
+    getUserToken,
+    getToken,
+    removeToken,
+}
---
+++ src/Services/userService.js
@@ -0,0 +1,22 @@
+function signup() {
+
+}
+
+function getUser() {
+
+}
+
+function login () {
+
+}
+
+function logout () {
+    
+}
+
+export {
+    signup,
+    getUser,
+    logout,
+    login,
+}
